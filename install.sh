b1d1cc5013f4cba8b60dcc55799d05829855f26c
Vinit Kumar <vinitcool76@gmail.com>
2012-05-18 13:15:11 +0530

    Version 0.1.2

---
+++ install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash 
+
+# This installs pastebinit to provide a web interface for System Information 
+
+sudo apt-get -y install pastebinit
+
+#This installs the necassary modules/dependancies
+
+sudo python setup.py install
+
+
