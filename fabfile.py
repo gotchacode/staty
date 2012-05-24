bf568e48702a3f4dd19d9ab077e75278d4a04d95
Vinit Kumar <vinitcool76@gmail.com>
2012-05-24 23:56:25 +0530

    random

--- fabfile.py
+++ fabfile.py
@@ -7,4 +7,4 @@ from fabric.api import local
 def prepare_commit():
     local("git add *")
     local("git commit -m 'random'")
-    local("git push -u origin master`")
+    local("git push -u origin master")
