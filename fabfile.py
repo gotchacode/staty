7c3fe64b6f24be5773bc95e4a7e3d25cf0581629
Vinit Kumar <vinitcool76@gmail.com>
2012-05-25 00:00:59 +0530

    random

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,8 @@
 #! /usr/bin/python
 
-
-from fabric.api import local
-
+from __future__ import with_statement
+from fabric.api import local,settings,abort
+from fabric.contrib.console import confirm
 
 def prepare_commit():
     local("git add *")
